a8cc6705afa81fcf01b9619f6c77d852b74eb30f
Praveen Kumar <91124025+praveenkumar277@users.noreply.github.com>
2023-05-24 08:51:34 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ pkg install python wget git
 pip install yt-dlp
 mkdir ~/bin/
 cd ~/bin/
-wget https://github.com/praveenkumar277/PyYTTD/blob/023574223c9115c781bbff062e5546d13e4d83c8/termux-url-opener
+wget https://raw.githubusercontent.com/praveenkumar277/PyYTTD/main/termux-url-opener
 chmod +x termux-url-opener
 cd -
 echo "Installation Finished "
