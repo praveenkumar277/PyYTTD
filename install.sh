c2d0d64d21ec27540df824647606f7b28ad2acd8
Praveen Kumar <91124025+praveenkumar277@users.noreply.github.com>
2023-09-27 19:44:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 termux-setup-storage
 pkg install python wget git
-pip install yt-dlp
+pip install yt-dlp colorama
 mkdir ~/bin/
 cd ~/bin/
 wget https://raw.githubusercontent.com/praveenkumar277/PyYTTD/main/termux-url-opener
